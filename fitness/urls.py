7250323bdc806c6c31d96a504c234684d426c3d2
Vincent Hong Lee <vlee747@berkeley.edu>
2021-08-04 17:37:57 -0700

    updated django imports

--- fitness/urls.py
+++ fitness/urls.py
@@ -6,7 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-    path('phone', views.phone, name='phone'),
-    path('phone2', views.phone2, name='phone2'),
 
 ]
