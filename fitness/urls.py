75ae6d947c406dc44122c88cef141ef9887859e3
Vincent Hong Lee <vlee747@berkeley.edu>
2021-08-07 16:32:30 -0700

    added html pages

--- fitness/urls.py
+++ fitness/urls.py
@@ -5,7 +5,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('motivation', views.motivation, name='motivation'),
+    path('motivation/', views.motivation, name='motivation'),
+    path('about/', views.about, name='about'),
+    path('achievements/', views.achievements, name='achievements'),
+    path('calendar/', views.calendar, name='calendar'),
+    path('catalog-exercise/', views.catalogExercise, name='catalog-exercise'),
+    path('goal-progress-archive/', views.goalProgressArchive, name='goal-progress-archive'),
+    path('muscle-groups/', views.muscleGroups, name='muscleGroups'),
+    path('progress/', views.progress, name='progress'),
     path('<str:room_name>/', views.room, name='room'),
 
 ]
--- fitness/views.py
+++ fitness/views.py
@@ -7,6 +7,30 @@ def index(request):
 def motivation(request):
     return render(request, 'fitness/motivation.html')
 
+def about(request):
+    return render(request, 'fitness/about.html')
+
+def achievements(request):
+    return render(request, 'fitness/achievements.html')
+
+def calendar(request):
+    return render(request, 'fitness/calendar.html')
+
+def catalogExercise(request):
+    return render(request, 'fitness/catalog-exercise.html')
+
+def goalProgressArchive(request):
+    return render(request, 'fitness/goal-progress-archive.html')
+
+def goals(request):
+    return render(request, 'fitness/goals.html')
+
+def muscleGroups(request):
+    return render(request, 'fitness/muscle-groups.html')
+
+def progress(request):
+    return render(request, 'fitness/progress.html')
+
 def room(request, room_name):
     return render(request, 'fitness/room.html', {
         'room_name': room_name
